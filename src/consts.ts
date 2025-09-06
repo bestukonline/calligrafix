9efe0c2a2409a8779381471eedf3810764dc55b2
Best UK <freshkid+bestukonlinecasino@gmail.com>
2025-09-06 10:11:47 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Calligrafix penmanship by design";
+export const SITE_DESCRIPTION = "The home of caligraphy and graphic design for UK word lovers";
